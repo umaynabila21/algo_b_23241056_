2f13caebbe95c220598eeb603f5062a62da16004
umaynabila21 <146051177+umaynabila21@users.noreply.github.com>
2024-01-02 14:09:07 +0700

    UAS_program perkalian 02/01/2024

---
+++ program perkalian/program perkalian.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void
+main(String[] args) {
+    perkalian(10,5);
+   }
+   
+     public static void
+     perkalian(int batas, int angka) {
+     for (int i = 1; i <= batas; i++) {
+        int hasil = i *
+    angka; 
+         System.out.println(i + " x " + angka + " = " + hasil);
+       }
+     }
+    }
